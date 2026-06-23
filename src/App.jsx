a2fd56736da10a61cb3e9f2e95f9c1e0515f35c3
Bishal Mahar <bishalmahar4@gmail.com>
2026-06-23 11:44:41 +0545

    checheckout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {HomePage} from './pages/HomePage.jsx'
+import {CheckoutPage} from './pages/CheckoutPage.jsx'
 import {Routes,Route} from 'react-router'
 import './App.css'
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="check" element={<div>Bishal</div>} />
+      <Route path="check" element={<CheckoutPage />} />
     </Routes>  
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,9 @@
  export function HomePage(){
   return(
     <>
+
+      <title> Ecommerce-Project</title>
+
       <div className="header">
         <div className="left-section">
           <a href="index.html" className="header-link">
