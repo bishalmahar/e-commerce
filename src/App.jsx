519d46d43fafcd36f2b36cdd78116fe01bad6aea
Bishal Mahar <bishalmahar4@gmail.com>
2026-06-25 09:16:49 +0545

    add new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import {HomePage} from './pages/HomePage.jsx'
 import {CheckoutPage} from './pages/CheckoutPage.jsx'
 import {Routes,Route} from 'react-router'
+import {OrdersPage} from './pages/OrdersPage.jsx'
 import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="check" element={<CheckoutPage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
     </Routes>  
   )
 }
