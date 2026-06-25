314391389421a1bfad7068e8ac415ab4a4e9bad6
Bishal Mahar <bishalmahar4@gmail.com>
2026-06-25 13:22:05 +0545

    tracking page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {HomePage} from './pages/HomePage.jsx'
 import {CheckoutPage} from './pages/CheckoutPage.jsx'
 import {Routes,Route} from 'react-router'
 import {OrdersPage} from './pages/OrdersPage.jsx'
+import {TrackingPage} from './pages/TrakingPage.jsx'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/Tracking" element={<TrackingPage />} />
     </Routes>  
   )
 }
